0a848e9fceff6db7b41fd98222e239bb02bf7e41
Osama-Mattar <osama01145@fci.zu.edu.eg>
2020-11-14 19:52:53 +0200

    Create story details view layer

--- app/src/main/java/com/mattar/nyt_top_stories/storydetails/StoryDetailsFragment.kt
+++ app/src/main/java/com/mattar/nyt_top_stories/storydetails/StoryDetailsFragment.kt
@@ -1,32 +1,53 @@
 package com.mattar.nyt_top_stories.storydetails
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.navArgs
+import coil.load
 import com.mattar.nyt_top_stories.R
+import com.mattar.nyt_top_stories.base.extension.observe
+import com.mattar.nyt_top_stories.base.fragment.BaseFragment
+import kotlinx.android.synthetic.main.story_details_fragment.*
 
-class StoryDetailsFragment : Fragment() {
+class StoryDetailsFragment : BaseFragment() {
 
-    companion object {
-        fun newInstance() = StoryDetailsFragment()
+    private val viewModel by viewModels<StoryDetailsViewModel>()
+    private val args: StoryDetailsFragmentArgs by navArgs()
+
+    private val stateObserver = Observer<StoryDetailsViewModel.ViewState> { viewState ->
+
+        storyImage.load(viewState.story!!.multimedia.filter { image -> image.format == "thumbLarge" }
+            .first().url) {
+            crossfade(true)
+            error(R.drawable.ic_image)
+            fallback(R.drawable.ic_image)
+        }
+        storyTitle.text = viewState.story.title
+        storyPublicationDate.text = viewState.story.published_date
+        storyAbstract.text = viewState.story.abstract
+        storyUrl.apply {
+            text = viewState.story.short_url
+            movementMethod = LinkMovementMethod.getInstance()
+        }
     }
-
-    private lateinit var viewModel: StoryDetailsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewModel.start(args.story)
         return inflater.inflate(R.layout.story_details_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(StoryDetailsViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observe(viewModel.stateLiveData, stateObserver)
+        viewModel.loadData()
     }
 
 }
--- app/src/main/java/com/mattar/nyt_top_stories/storydetails/StoryDetailsViewModel.kt
+++ app/src/main/java/com/mattar/nyt_top_stories/storydetails/StoryDetailsViewModel.kt
@@ -1,7 +1,59 @@
 package com.mattar.nyt_top_stories.storydetails
 
-import androidx.lifecycle.ViewModel
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
+import com.mattar.nyt_top_stories.base.viewmodel.BaseAction
+import com.mattar.nyt_top_stories.base.viewmodel.BaseViewModel
+import com.mattar.nyt_top_stories.base.viewmodel.BaseViewState
+import com.mattar.nyt_top_stories.data.Result
+import com.mattar.nyt_top_stories.data.model.Story
 
-class StoryDetailsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+internal class StoryDetailsViewModel @ViewModelInject constructor() :
+    BaseViewModel<StoryDetailsViewModel.ViewState, StoryDetailsViewModel.Action>(ViewState()) {
+
+    private val _story = MutableLiveData<Story>()
+
+    fun start(story: Story) {
+        _story.value = story
+    }
+
+    override fun onLoadData() {
+        val result = Result.Success(_story.value)
+        val action = Action.StoryLoadingSuccess(result.data!!)
+
+        sendAction(action)
+    }
+
+    override fun onReduceState(viewAction: Action): ViewState =
+        when (viewAction) {
+            is Action.StoryLoading -> state.copy(
+                isLoading = true,
+                isError = false,
+                story = null
+            )
+            is Action.StoryLoadingSuccess -> state.copy(
+                isLoading = false,
+                isError = false,
+                story = viewAction.story
+            )
+            is Action.StoryLoadingFailure -> state.copy(
+                isLoading = false,
+                isError = true,
+                story = null
+            )
+        }
+
+    internal data class ViewState(
+        val isLoading: Boolean = true,
+        val isError: Boolean = false,
+        val story: Story? = null
+    ) : BaseViewState
+
+    internal sealed class Action : BaseAction {
+        class StoryLoadingSuccess(val story: Story) : Action()
+        object StoryLoadingFailure : Action()
+        object StoryLoading : Action()
+    }
+
+
 }
