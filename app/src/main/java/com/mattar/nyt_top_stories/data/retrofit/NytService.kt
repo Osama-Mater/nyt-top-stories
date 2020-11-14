f543393f8eaa34937ad96b48dc6c04bd35e360b8
Osama-Mattar <osama01145@fci.zu.edu.eg>
2020-11-14 13:39:13 +0200

    Nyt Top Stories

--- app/src/main/java/com/mattar/nyt_top_stories/data/retrofit/NytService.kt
+++ app/src/main/java/com/mattar/nyt_top_stories/data/retrofit/NytService.kt
@@ -1,4 +1,9 @@
 package com.mattar.nyt_top_stories.data.retrofit
 
+import com.mattar.nyt_top_stories.data.model.TopStories
+import retrofit2.http.GET
+
 interface NytService {
+    @GET("/home.json")
+    suspend fun fetchNytTopStories(): TopStories
 }
