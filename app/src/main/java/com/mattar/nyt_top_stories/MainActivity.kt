40d42b08ad61efbad158ec14cf9647d5207bc986
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-08-26 01:28:31 +0200

    use viewBinding for MainActivity.kt

--- app/src/main/java/com/mattar/nyt_top_stories/MainActivity.kt
+++ app/src/main/java/com/mattar/nyt_top_stories/MainActivity.kt
@@ -5,19 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.mattar.nyt_top_stories.databinding.ActivityMainBinding
+import com.mattar.nyt_top_stories.utils.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMainBinding::inflate)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController: NavController = navHostFragment.navController
-        bottomNav.setupWithNavController(navController)
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
