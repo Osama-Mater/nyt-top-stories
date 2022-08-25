c37e356c80ee19d451030b3470b15dbb2c69f637
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-08-26 01:30:27 +0200

    use viewBinding for FavouriteStoriesFragment.kt

--- app/src/main/java/com/mattar/nyt_top_stories/favourites/FavouriteStoriesFragment.kt
+++ app/src/main/java/com/mattar/nyt_top_stories/favourites/FavouriteStoriesFragment.kt
@@ -1,23 +1,14 @@
 package com.mattar.nyt_top_stories.favourites
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.mattar.nyt_top_stories.R
 import com.mattar.nyt_top_stories.base.fragment.BaseFragment
+import com.mattar.nyt_top_stories.databinding.FavouriteStoriesFragmentBinding
+import com.mattar.nyt_top_stories.utils.viewBinding
 
 class FavouriteStoriesFragment : BaseFragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.favourite_stories_fragment, container, false)
-    }
+    private val binding by viewBinding(FavouriteStoriesFragmentBinding::bind)
 
     override fun onResume() {
         super.onResume()
-        underConstructionAnimation.playAnimation()
+        binding.underConstructionAnimation.playAnimation()
     }
 }
