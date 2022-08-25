f95d854aa07fd30c9946126e466ebe197c977344
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-08-26 01:47:46 +0200

    update Parcelize package

--- app/src/main/java/com/mattar/nyt_top_stories/data/model/Story.kt
+++ app/src/main/java/com/mattar/nyt_top_stories/data/model/Story.kt
@@ -1,7 +1,7 @@
 package com.mattar.nyt_top_stories.data.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Story(
