2650287d3584441400eb931c950a41feeabc4b44
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-08-26 02:27:08 +0200

    fix the view & animation for FavouriteStoriesFragment

--- app/src/main/java/com/mattar/nyt_top_stories/favourites/FavouriteStoriesFragment.kt
+++ app/src/main/java/com/mattar/nyt_top_stories/favourites/FavouriteStoriesFragment.kt
@@ -1,5 +1,10 @@
 package com.mattar.nyt_top_stories.favourites
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mattar.nyt_top_stories.R
 import com.mattar.nyt_top_stories.base.fragment.BaseFragment
 import com.mattar.nyt_top_stories.databinding.FavouriteStoriesFragmentBinding
 import com.mattar.nyt_top_stories.utils.viewBinding
@@ -7,8 +12,18 @@ import com.mattar.nyt_top_stories.utils.viewBinding
 class FavouriteStoriesFragment : BaseFragment() {
     private val binding by viewBinding(FavouriteStoriesFragmentBinding::bind)
 
-    override fun onResume() {
-        super.onResume()
-        binding.underConstructionAnimation.playAnimation()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.favourite_stories_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            underConstructionAnimation.playAnimation()
+        }
     }
 }
