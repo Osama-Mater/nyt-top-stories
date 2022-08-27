99f6efd3a4e9be41c5243d3a10cba4aebb25e017
Osama Ahmed Mattar <osama01145@fci.zu.edu.eg>
2022-08-27 22:48:03 +0200

    Show the toolbar and add the up navigation action

--- app/src/main/java/com/mattar/nyt_top_stories/MainActivity.kt
+++ app/src/main/java/com/mattar/nyt_top_stories/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.mattar.nyt_top_stories.databinding.ActivityMainBinding
 import com.mattar.nyt_top_stories.utils.viewBinding
@@ -12,15 +13,19 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val binding by viewBinding(ActivityMainBinding::inflate)
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        supportActionBar?.hide()
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController: NavController = navHostFragment.navController
+        navController = navHostFragment.navController
         binding.bottomNav.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
